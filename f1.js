59b4d3b2022d26474dd17f3fd8f24f649dddcd5f
idontbyte <tanveer.cse69@gmail.com>
2024-07-31 21:09:02 +0600

    Learned about function expressions in JS

--- f1.js
+++ f1.js
@@ -1,4 +1,9 @@
-(function sum(num1, num2){
+// (function sum(num1, num2){
+//     var result = num1 + num2;
+//     document.write("Result = "+ result);
+// })(5,10);
+const display1 = function sum(num1, num2){
     var result = num1 + num2;
     document.write("Result = "+ result);
-})(5,10);
+}
+display1(5,30);
