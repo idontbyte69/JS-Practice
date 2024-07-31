0a26694fee1010070d2e83ce5f09a02c4f7da380
idontbyte <tanveer.cse69@gmail.com>
2024-07-31 20:58:48 +0600

    learned about function in JS and make a Calculator using JS

---
+++ calc.js
@@ -0,0 +1,9 @@
+function addition(num1, num2){
+    var result = num1+num2;
+    return result;
+}
+
+var num1 = Number(prompt("Enter 1st Number: "));
+var num2 = Number(prompt("Enter 2nd Number: "));
+
+document.write("Result of addition : "+ addition(num1, num2));
---
+++ f1.js
@@ -0,0 +1,6 @@
+function sq(){
+    var num = 5;
+    var result = num ** 2;
+    document.write("Result = "+ result);
+}
+sq();
