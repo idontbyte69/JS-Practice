4b885f2844457578365ca701d865becca5febccc
idontbyte <tanveer.cse69@gmail.com>
2024-07-31 21:17:04 +0600

    Learning Array

---
+++ array.js
@@ -0,0 +1,12 @@
+var dept = new Array();
+
+dept[0] = "CSE";
+dept[1] = "EEE";
+dept[2] = "TEX";
+dept[3] = "GBS";
+dept[4] = "SOC";
+dept[5] = "ENG";
+
+
+document.write(dept[0] + "<br/>");
+document.write(dept[3] + "<br/>");
