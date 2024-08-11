d003bed10f9274bb824f7491051ec41252a58ab7
idontbyte <tanveer.cse69@gmail.com>
2024-08-12 01:20:22 +0600

    Learned Number sortinh

---
+++ array_loop.js
@@ -0,0 +1,4 @@
+var num = [0,1,6,9,10,123];
+for(var i = 0; i<5; i++){
+    document.write(num[i] + "<br/>");
+}
---
+++ numberArraySorting.js
@@ -0,0 +1,5 @@
+var numbers = [20,30,5,6,8,123,3];
+numbers.sort(function(a,b){
+    return a-b;
+});
+document.write(numbers);
