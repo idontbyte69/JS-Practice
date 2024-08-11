ee95e24d64b394a668bf65999c31ad0c8328da3d
idontbyte <tanveer.cse69@gmail.com>
2024-08-12 01:40:11 +0600

    comit

--- numberArraySorting.js
+++ numberArraySorting.js
@@ -1,5 +1,26 @@
-var numbers = [20,30,5,6,8,123,3];
-numbers.sort(function(a,b){
-    return a-b;
-});
-document.write(numbers);
+// var numbers = [20,30,5,6,8,123,3];
+// numbers.sort(function(a,b){
+//     return a-b;
+// });
+// document.write(numbers);
+
+function highestScore (arr1){
+    var hs = arr1[0];
+    for (var i = 0; i < 5; i++ ){
+        if(hs < arr1[i]){
+            hs = arr1[i];
+        }
+    }
+
+return hs;
+}
+
+var arr1 = new Array();
+var n = 5;
+for (var i = 0; i < n ; i++){
+    arr1[i] = Number(prompt("Enter a number: "));
+    // arr1.push(input);
+};
+
+var highestScore = highestScore(arr1);
+document.write(highestScore);
